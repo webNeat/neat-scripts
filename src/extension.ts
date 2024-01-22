1ba064eeb7ba769d09ba02f1543f1daeed0adffc
Amine Ben hammou <webneat@gmail.com>
2024-01-22 01:47:10 +0100

    Rename package to neat-scripts

--- src/extension.ts
+++ src/extension.ts
@@ -2,8 +2,8 @@ import * as vscode from 'vscode'
 import * as commands from './commands'
 
 export function activate(context: vscode.ExtensionContext) {
-  context.subscriptions.push(vscode.commands.registerCommand('scripts.run', commands.run))
-  context.subscriptions.push(vscode.commands.registerCommand('scripts.files.open', commands.files.open))
+  context.subscriptions.push(vscode.commands.registerCommand('neat-scripts.run', commands.run))
+  context.subscriptions.push(vscode.commands.registerCommand('neat-scripts.files.open', commands.files.open))
 }
 
 export function deactivate() {}
